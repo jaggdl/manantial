2f4c891832d47b257c6f88a4f21a824d70408767
jaggdl <ja125garcia@gmail.com>
2024-06-21 02:59:40 -0600

    Revert "Styling improvements"
    This reverts commit 19d26568048b16533eaf3965f4cda24fe3f7a749.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,2 @@
 module ApplicationHelper
-  def active_link_to(name, path)
-    if path == '/'
-      class_name = request.path == '/' ? 'font-semibold' : ''
-    else
-      class_name = request.path.start_with?(path) ? 'font-semibold' : ''
-    end
-    link_to name, path, class: class_name
-  end
 end
