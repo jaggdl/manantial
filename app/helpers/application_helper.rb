ddd742591bffb97086609f4d518b3b538f173aa1
jaggdl <ja125garcia@gmail.com>
2024-06-21 03:01:04 -0600

    Layout refactor

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def active_link_to(name, path)
+    if path == '/'
+      class_name = request.path == '/' ? 'font-semibold' : ''
+    else
+      class_name = request.path.start_with?(path) ? 'font-semibold' : ''
+    end
+    link_to name, path, class: class_name
+  end
 end
