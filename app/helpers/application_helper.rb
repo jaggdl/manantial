38fec07e5f16388def77193adcab645641c0896e
jaggdl <ja125garcia@gmail.com>
2024-06-21 03:49:40 -0600

    Blog UI improvements

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,27 @@
 module ApplicationHelper
+  TIME_PERIODS = [
+    { unit: "year",   seconds: 31_536_000 },
+    { unit: "month",  seconds: 2_592_000 },
+    { unit: "day",    seconds: 86_400 },
+    { unit: "hour",   seconds: 3_600 },
+    { unit: "minute", seconds: 60 },
+    { unit: "second", seconds: 1 }
+  ].freeze
+
+  def time_since_created(created_at)
+    time_difference = Time.now - created_at
+
+    TIME_PERIODS.each do |period|
+      if time_difference >= period[:seconds]
+        value = (time_difference / period[:seconds]).to_i
+        unit = value == 1 ? period[:unit] : period[:unit] + 's'
+        return "#{value} #{unit} ago"
+      end
+    end
+
+    "just now"
+  end
+
   def active_link_to(name, path)
     if path == '/'
       class_name = request.path == '/' ? 'font-semibold' : ''
