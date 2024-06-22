905c4e89d748bb11946ff0d6865720231cbdf45d
jaggdl <ja125garcia@gmail.com>
2024-06-21 19:57:55 -0600

    Adds i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,20 @@
 class ApplicationController < ActionController::Base
+  around_action :switch_locale
   before_action :redirect_to_signup_if_no_users, only: :index
 
   def index
   end
 
   private
+
+  def default_url_options
+    { locale: I18n.locale == :en ? nil : I18n.locale }
+  end
+
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
 
   def redirect_to_signup_if_no_users
     if User.count.zero?
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  root "application#index"
+  scope "(:locale)", locale: /en|es/ do
+    # Defines the root path route ("/")
+    root "application#index"
 
-  resources :posts, path: 'blog', param: :id
+    resources :posts, path: 'blog', param: :id
+  end
+
+  # Define routes without locale
+  root to: 'application#index', as: :unlocalized_root
+
   resources :users, only: [:new, :create]
 end
