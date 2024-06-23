0b52e98683e72ef7d27dd589982c32e7decb6ca0
jaggdl <ja125garcia@gmail.com>
2024-06-22 19:51:45 -0600

    Adds og:locale meta tag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,6 +8,11 @@ module ApplicationHelper
     { unit: "second", seconds: 1 }
   ].freeze
 
+  LOCALES_WITH_TERRITORY = {
+    :en => 'en_US',
+    :es => 'es_MX',
+  }.freeze
+
   def time_since_created(created_at)
     time_difference = Time.now - created_at
 
@@ -20,5 +25,9 @@ module ApplicationHelper
     end
 
     "just now"
+  end
+
+  def locale_with_territory(locale = nil)
+    LOCALES_WITH_TERRITORY[locale || I18n.locale]
   end
 end
