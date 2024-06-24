ebadf9b7a22cc9630b2d7fb2c7d571c755c9df26
jaggdl <ja125garcia@gmail.com>
2024-06-24 04:45:54 -0600

    Optimizes post images in next-gen formats

--- app/uploaders/post_image_uploader.rb
+++ app/uploaders/post_image_uploader.rb
@@ -3,6 +3,35 @@ class PostImageUploader < CarrierWave::Uploader::Base
 
   storage :file
 
+  VERSIONS = {
+    xs: [480, nil],
+    sm: [640, nil],
+    md: [768, nil],
+    lg: [1024, nil],
+    xl: [1280, nil],
+    xxl: [1536, nil]
+  }
+
+  VERSIONS.each do |version_name, dimensions|
+    version version_name do
+      process resize_to_fit: dimensions
+
+      version :avif do
+        process convert: 'avif'
+        def full_filename(for_file)
+          "#{version_name}_#{File.basename(for_file, '.*')}.avif"
+        end
+      end
+
+      version :webp do
+        process convert: 'webp'
+        def full_filename(for_file)
+          "#{version_name}_#{File.basename(for_file, '.*')}.webp"
+        end
+      end
+    end
+  end
+
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
@@ -11,39 +40,7 @@ class PostImageUploader < CarrierWave::Uploader::Base
     original_filename
   end
 
-  def self.version_sizes
-    {
-      xs: [480, nil],
-      sm: [640, nil],
-      md: [768, nil],
-      lg: [1024, nil],
-      xl: [1280, nil],
-      xxl: [1536, nil]
-    }
-  end
-
-  version_sizes.each do |version, size|
-    version version do
-      process resize_to_fit: size
-      %w[webp avif].each do |format|
-        version format do
-          process convert: format
-          process set_content_type: format
-          define_method(:full_filename) do |for_file|
-            "#{for_file.chomp(File.extname(for_file))}.#{format}"
-          end
-        end
-      end
-    end
-  end
-
   def extension_allowlist
     %w[jpg jpeg png]
-  end
-
-  private
-
-  def set_content_type(format)
-    file.content_type = "image/#{format}"
   end
 end
