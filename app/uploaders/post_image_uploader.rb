b6a8d53560b1bc3c95683dd592c384c63ec83c57
jaggdl <ja125garcia@gmail.com>
2024-06-24 04:15:58 -0600

    Optimizes post images

--- app/uploaders/post_image_uploader.rb
+++ app/uploaders/post_image_uploader.rb
@@ -11,31 +11,39 @@ class PostImageUploader < CarrierWave::Uploader::Base
     original_filename
   end
 
-  version :xs do
-    process resize_to_fit: [480, nil]
+  def self.version_sizes
+    {
+      xs: [480, nil],
+      sm: [640, nil],
+      md: [768, nil],
+      lg: [1024, nil],
+      xl: [1280, nil],
+      xxl: [1536, nil]
+    }
   end
 
-  version :sm do
-    process resize_to_fit: [640, nil]
-  end
-
-  version :md do
-    process resize_to_fit: [768, nil]
-  end
-
-  version :lg do
-    process resize_to_fit: [1024, nil]
-  end
-
-  version :xl do
-    process resize_to_fit: [1280, nil]
-  end
-
-  version :xxl do
-    process resize_to_fit: [1536, nil]
+  version_sizes.each do |version, size|
+    version version do
+      process resize_to_fit: size
+      %w[webp avif].each do |format|
+        version format do
+          process convert: format
+          process set_content_type: format
+          define_method(:full_filename) do |for_file|
+            "#{for_file.chomp(File.extname(for_file))}.#{format}"
+          end
+        end
+      end
+    end
   end
 
   def extension_allowlist
     %w[jpg jpeg png]
+  end
+
+  private
+
+  def set_content_type(format)
+    file.content_type = "image/#{format}"
   end
 end
