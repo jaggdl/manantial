193e73d601c22d430b94625b2bfe70dedbecbf86
jaggdl <ja125garcia@gmail.com>
2024-06-23 19:16:46 -0600

    improves post form styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,31 +1,34 @@
 module ApplicationHelper
   TIME_PERIODS = [
-    { unit: "year",   seconds: 31_536_000 },
-    { unit: "month",  seconds: 2_592_000 },
-    { unit: "day",    seconds: 86_400 },
-    { unit: "hour",   seconds: 3_600 },
-    { unit: "minute", seconds: 60 },
-    { unit: "second", seconds: 1 }
+    { unit: :year,   seconds: 31_536_000 },
+    { unit: :month,  seconds: 2_592_000 },
+    { unit: :day,    seconds: 86_400 },
+    { unit: :hour,   seconds: 3_600 },
+    { unit: :minute, seconds: 60 },
+    { unit: :second, seconds: 1 }
   ].freeze
+
+  def time_since_created(created_at)
+    time_difference = Time.now - created_at
+
+    I18n.with_locale(I18n.locale) do
+      TIME_PERIODS.each do |period|
+        if time_difference >= period[:seconds]
+          value = (time_difference / period[:seconds]).to_i
+          unit_key = value == 1 ? period[:unit] : "#{period[:unit]}s".to_sym
+          unit = I18n.t("time_periods.#{unit_key}")
+          return I18n.t('time_since.ago', value: value, unit: unit)
+        end
+      end
+
+      I18n.t('time_since.just_now')
+    end
+  end
 
   LOCALES_WITH_TERRITORY = {
     :en => 'en_US',
     :es => 'es_MX',
   }.freeze
-
-  def time_since_created(created_at)
-    time_difference = Time.now - created_at
-
-    TIME_PERIODS.each do |period|
-      if time_difference >= period[:seconds]
-        value = (time_difference / period[:seconds]).to_i
-        unit = value == 1 ? period[:unit] : period[:unit] + 's'
-        return "#{value} #{unit} ago"
-      end
-    end
-
-    "just now"
-  end
 
   def locale_with_territory(locale = nil)
     LOCALES_WITH_TERRITORY[locale || I18n.locale]
