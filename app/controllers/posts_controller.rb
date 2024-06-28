21fa52a6a5aa40489ca29cba824a4b75b719dc7f
jaggdl <ja125garcia@gmail.com>
2024-06-28 00:10:38 -0600

    Disallows visitor traking on og_image endpoint

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy, :og_image]
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  skip_before_action :track_ahoy_visit, only: [:og_image]
 
   def index
     @posts = Post.order(created_at: :desc).select(&:translated?)
