b6aa000fab053a014fa6a965e38c1d95ac53019a
jaggdl <ja125garcia@gmail.com>
2024-07-05 04:22:17 -0600

    Adds connections mentions in markdown

--- app/helpers/markdown_renderer_with_special_links.rb
+++ app/helpers/markdown_renderer_with_special_links.rb
@@ -1,14 +1,20 @@
 class MarkdownRendererWithSpecialLinks < Redcarpet::Render::HTML
+  def preprocess(full_document)
+    updated_document = full_document.gsub(/(^|\s)@([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})(\s|$)/) do |match|
+      prefix, hostname, suffix = $1, $2, $3
+      "#{prefix}#{hostname_link(hostname)}#{suffix}"
+    end
+    updated_document
+  end
+
   def autolink(link, link_type)
     case link_type
-      when :url then url_link(link)
-      when :email then email_link(link)
+    when :url then url_link(link)
+    when :email then email_link(link)
     end
   end
 
   def url_link(link)
-    puts "url_link LINK: #{link} #{link.include?('youtube.com')}"
-
     case link
     when /youtube\.com/
       youtube_link(link)
@@ -26,6 +32,31 @@ class MarkdownRendererWithSpecialLinks < Redcarpet::Render::HTML
     else
       "Invalid YouTube URL"
     end
+  end
+
+  def hostname_link(hostname)
+
+    connection = Connection::Set.find_by(domain: hostname)
+
+    unless connection.present?
+      return render_chip(nil, hostname)
+    end
+
+    response = HTTParty.get("https://#{hostname}/api/v1/connection/public_info")
+    if response.success?
+      data = response.parsed_response
+      profile_picture = data['profile_picture']
+      name = data['name']
+      render_chip(profile_picture, name)
+    else
+      "Invalid hostname or failed to fetch data"
+    end
+  rescue StandardError => e
+    "Error fetching data: #{e.message}"
+  end
+
+  def render_chip(profile_picture, name)
+    render_partial('profiles/chip', profile_picture:, name:)
   end
 
   def normal_link(link)
