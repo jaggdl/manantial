3d2912d62cfd01666dd2ce0ec09331062fddaeda
jaggdl <ja125garcia@gmail.com>
2024-07-11 22:14:52 -0600

    Post feed fixes

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -4,6 +4,10 @@ class FeedController < ApplicationController
 
     @latest_posts = connection_sets.map(&:latest_posts).flatten
 
+    @latest_posts = @latest_posts.map do |post|
+      OpenStruct.new(post)
+    end
+
     @latest_posts.sort_by!(&:created_at).reverse!
   end
 end
