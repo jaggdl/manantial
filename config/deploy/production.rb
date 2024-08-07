0bb9ff85afc9a592d1705369c4b5117618e14987
jaggdl <ja125garcia@gmail.com>
2024-08-07 00:08:44 -0600

    Shows message when there are no public posts

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 server 'manantial.jaggdl.com', user: 'deploy', roles: %w{production app db web}
-server 'jaggdl.com', user: 'deploy', roles: %w{production app db web}
+# server 'jaggdl.com', user: 'deploy', roles: %w{production app db web}
 
 
 
