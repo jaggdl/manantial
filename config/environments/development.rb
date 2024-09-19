db5517a699bb56a2c32dcaa965c5e6d42b515a73
jaggdl <ja125garcia@gmail.com>
2024-09-20 00:53:37 +0200

    Revert "Rails upgrade"
    This reverts commit 2c8b2073c8893074105c6c225b6d86559841748f.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,5 +70,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  config.active_job.queue_adapter = :async
+
   config.hosts = ['jaggdl.dev', 'localhost:4201', 'chano.dev', 'localhost:4202', 'memo.dev']
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_19_214845) do
+ActiveRecord::Schema[7.0].define(version: 2024_08_07_044224) do
   create_table "ahoy_events", force: :cascade do |t|
     t.integer "visit_id"
     t.integer "user_id"
@@ -149,4 +149,5 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_19_214845) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
 end
