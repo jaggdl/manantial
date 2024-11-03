39baa8672b6d8e25ef557f5972d76c1a58def2f7
jaggdl <ja125garcia@gmail.com>
2024-11-02 23:48:40 -0600

    Update Rails to 7.1

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,10 @@ module RailsBootstrapNoBuild
 
     config.i18n.available_locales = [:en, :es]
     config.i18n.default_locale = :en
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w(assets tasks))
 
     # Configuration for the application, engines, and railties goes here.
     #
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_19_214845) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_03_052047) do
   create_table "ahoy_events", force: :cascade do |t|
     t.integer "visit_id"
     t.integer "user_id"
