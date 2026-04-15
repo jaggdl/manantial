53e5cf58b50e6155905303569af3d79a2bace7a5
Jose Alfredo <ja125garcia@gmail.com>
2026-04-15 14:19:47 -0600

    Show Lexxy toolbar

--- app/javascript/controllers/post_form_controller.js
+++ app/javascript/controllers/post_form_controller.js
@@ -7,16 +7,5 @@ export default class extends Controller {
   selectType(event) {
     const type = event.params.type
     this.element.dataset.postType = type
-    this.toggleToolbar(type)
-  }
-
-  toggleToolbar(type) {
-    if (this.hasEditorTarget) {
-      if (type === "post") {
-        this.editorTarget.setAttribute("toolbar", "false")
-      } else {
-        this.editorTarget.removeAttribute("toolbar")
-      }
-    }
   }
 }
