2205cda2d59d8636a94fc78510740185cfebfbbf
Jose Alfredo <ja125garcia@gmail.com>
2026-08-19 15:35:59 -0600

    Crop favicon avatar to square

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,6 +6,6 @@ module ApplicationHelper
   def owner_avatar_url
     return nil unless Current.owner&.avatar&.attached?
 
-    url_for(Current.owner.avatar.variant(resize_to_limit: [64, 64]))
+    url_for(Current.owner.avatar.variant(resize_to_fill: [64, 64]))
   end
 end
