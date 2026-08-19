e017a66269992c7e74b4691377f22ee3808f1826
Jose Alfredo <ja125garcia@gmail.com>
2026-08-19 15:27:20 -0600

    Use owner avatar as favicon

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,10 @@ module ApplicationHelper
   def base_url
     ENV.fetch("BASE_URL", "https://manantial.jaggdl.com")
   end
+
+  def owner_avatar_url
+    return nil unless Current.owner&.avatar&.attached?
+
+    url_for(Current.owner.avatar.variant(resize_to_limit: [64, 64]))
+  end
 end
